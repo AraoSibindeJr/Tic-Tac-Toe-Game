c4be15bd2d592c98613cd6ceb9c55edc0fb8b220
AraoSibindeJr <araosibinde098junior@gmail.com>
2024-12-19 23:03:48 +0200

    feat: metodo action Perfomed adicionado

--- TicTacToe/src/TicTacToe.java
+++ TicTacToe/src/TicTacToe.java
@@ -6,9 +6,15 @@ public class TicTacToe {
     int boardWidth = 600;
     int boardHeigth = 650;
 
-    JFrame frame = new JFrame();
+    JFrame frame = new JFrame("Tic-Tac-Toe do Araozinn");
     JLabel textLabel = new JLabel();
     JPanel textPanel = new JPanel();
+    JPanel boardPanel = new JPanel();
+
+    JButton[][] board = new JButton[3][3];
+    String playerX = "X";
+    String playerO = "O";
+    String currentPlayer = playerX;
 
     TicTacToe(){
         frame.setVisible(true);
@@ -20,8 +26,40 @@ public class TicTacToe {
 
         textLabel.setBackground(Color.DARK_GRAY);
         textLabel.setForeground(Color.white);
-        textLabel.setFont(new Font("Arial", Font.BOLD, 50));
+        textLabel.setFont(new Font("Arial", Font.BOLD, 40));
         textLabel.setHorizontalAlignment(JLabel.CENTER);
-        textLabel.setText("Tic-Tac-Toe do ARAOZINX");
+        textLabel.setText("Tic-Tac-Toe do Araozin");
+        textLabel.setOpaque(true);
+
+        textPanel.setLayout(new BorderLayout());
+        textPanel.add(textLabel);
+        frame.add(textPanel, BorderLayout.NORTH);
+
+        boardPanel.setLayout(new GridLayout(3,3));
+        boardPanel.setBackground(Color.DARK_GRAY);
+        frame.add(boardPanel);
+
+        for(int r = 0;r < 3;r++ ){
+            for(int c = 0; c<3; c++){
+                JButton tile = new JButton();
+                board [r][c] = tile;
+                boardPanel.add(tile);
+
+                tile.setBackground(Color.DARK_GRAY);
+                tile.setForeground(Color.WHITE);
+                tile.setFont(new Font("Arial",Font.BOLD,120));
+                tile.setFocusable(false);
+                //tile.setText(currentPlayer);
+
+                tile.addActionListener(new ActionListener() {
+                    @Override
+                    public void actionPerformed(ActionEvent e) {
+                        JButton tile = (JButton) e.getSource();
+                        tile.setText(currentPlayer);
+
+                    }
+                });
+            }
+        }
     }
 }
